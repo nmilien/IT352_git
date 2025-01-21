47ef031fe755f74869e05b906e2a62774a069919
aroussely <rousselyaugustin@gmail.com>
2025-01-21 09:57:32 +0100

    printf

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "mbed.h"
+#include "stdio.h"
 
 
 // Blinking rate in milliseconds
@@ -12,15 +13,17 @@
 
 int main()
 {
+    printf("Je suis PAS un BG");
     // Initialise the digital pin LED1 as an output
 #ifdef LED1
     DigitalOut led(LED1);
 #else
     bool led;
 #endif
-
+    ThisThread::sleep_for(1000);
     while (true) {
         led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
+        printf("Je suis un BG \n"); 
     }
 }
